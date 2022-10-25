4c785c50508f1bd7b2ae88c18c5c21342add7965
thisisfrostfang <xiiibale@gmail.com>
2022-10-25 11:57:19 +0800

    initial commit

---
+++ src/main/java/com/dti/finalproject/group3/walletapi/WalletApiApplication.java
@@ -0,0 +1,13 @@
+package com.dti.finalproject.group3.walletapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletApiApplication.class, args);
+	}
+
+}
